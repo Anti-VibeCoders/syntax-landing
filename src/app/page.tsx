377d06c6e3ebfa013bfcd8fe595b2845d50e902f
Lobosanplay <parra.santiago2008@gmail.com>
2025-10-08 19:36:03 -0500

    feat: logo and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/layout/header";
 import Hero from "@/components/sections/Hero";
-
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <Hero />
+      <Header />
+        <Hero />
     </main>
   );
 }
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const Header = () => {
    return(
     <div className="fixed w-full flex items-center justify-center">
-        <header className="flex items-center justify-center bg-gray-700/30 p-2 px-4 border border-gray-500 rounded-full w-100 ">
+        <header className="flex items-center justify-center bg-[#7E787810] p-2 px-4 border border-gray-800 rounded-full w-105 text-[#C0C0C0] font-semibold">
             <nav className="flex">
-                <ul className="flex gap-2">
-                    <li className="sm:mr-10"><img src="" alt="Logo" /></li>
+                <ul className="flex gap-2 items-center">
+                    <li className="sm:mr-6"><img src="/Logo-sintax.png" alt="Logo" className="h-10 w-10"/></li>
                     <li><a href="Inicio">Inicio</a></li>
                     <li><a href="Proyectos">Proyectos</a></li>
                     <li><a href="Nosotros">Nosotros</a></li>
